90969d6cd588099ad21ff6b02e24d61c00eb96c4
sung-da <desung2020@naver.com>
2021-11-10 17:12:45 +0900

    2021-11-10

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("hi");
+/* #2.3
+const a = 5;
+const b = 2;
+let myName = "sung";
+
+// const는 상수로 바뀌지 않는 값
+// let 은 새로 생성할 때만 사용
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "dada"; 
+
+console.log("your name is " + myName);
+ */
+
+/* #2.4
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something);
+*/
