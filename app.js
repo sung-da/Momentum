7eb52c0a7ccdb14cc68274b1dcf180db564726ab
sung-da <desung2020@naver.com>
2021-11-11 16:06:46 +0900

    2021-11-11

--- app.js
+++ app.js
@@ -22,3 +22,17 @@ let something;
 console.log(amIFat);
 console.log(something);
 */
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dayOfWeek = [mon , tue , wed , thu , fri , sat , sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "dada"]
+
+console.log(dayOfWeek, nonsense);
