99770c80e38431b14742ba6dea5b98cac7e7792a
sung-da <desung2020@naver.com>
2021-12-06 16:32:37 +0900

    2021-12-06 #4.7

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+// string만 포함된 변수는 대문자로 표기
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  // const username = loginInput.value;
-  event.preventDefault();
   // preventDefault를 호출하면 브라우저의 기본동작을 막아줌
-  console.log(loginInput.value);
+  event.preventDefault();
+  // hidden이라는 class name을 줘서 form 숨김
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  // 입력받은 input 값을 username(변수)에 저장
+  const username = loginInput.value;
+
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-// submit 감지(submit event)
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  // username을 h1에 텍스트 추가
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// localStorage 정보 유무 확인
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  // submit 감지(submit event)
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the grretings
+  paintGreetings(savedUsername);
+}
